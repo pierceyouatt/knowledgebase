e4fb076f6cbdde53c5a9b9df36bc50cd88c34d2d
Raghu Betina <raghu@firstdraft.com>
2018-10-20 07:48:43 +0000

    Direct association between project_types and proj_phases

--- app/models/proj_phase.rb
+++ app/models/proj_phase.rb
@@ -1,6 +1,9 @@
 class ProjPhase < ApplicationRecord
   # Direct associations
 
+  belongs_to :type,
+             :class_name => "ProjectType"
+
   # Indirect associations
 
   # Validations
--- app/models/project_type.rb
+++ app/models/project_type.rb
@@ -1,6 +1,9 @@
 class ProjectType < ApplicationRecord
   # Direct associations
 
+  has_many   :proj_phases,
+             :foreign_key => "type_id"
+
   belongs_to :sol,
              :class_name => "Solution"
 
