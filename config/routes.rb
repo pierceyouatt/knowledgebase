2f697618a7cf25b6bc68446c004ebecbfeb294c8
Raghu Betina <raghu@firstdraft.com>
2018-10-20 07:49:01 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "institutions#index"
   # Routes for the Proj phase resource:
 
