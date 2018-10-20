9c91da479f9e75943448bac6e17af1f23da41bcf
Raghu Betina <raghu@firstdraft.com>
2018-10-20 07:49:04 +0000

    Whitelist columns in Employee for ActiveAdmin

--- app/admin/employees.rb
+++ app/admin/employees.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Employee do
+
+ permit_params :email, :password, :name, :position
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
