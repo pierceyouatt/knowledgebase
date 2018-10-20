cd9df0574e43740375f09391aadfb809fe231187
Raghu Betina <raghu@firstdraft.com>
2018-10-20 07:49:13 +0000

    Whitelist columns in Engagement for ActiveAdmin

--- app/admin/engagements.rb
+++ app/admin/engagements.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Engagement do
+
+ permit_params :empl_id, :inst_id, :sol_id, :eng_name, :eng_number, :sp_link, :eng_summary, :type_id, :phase_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
