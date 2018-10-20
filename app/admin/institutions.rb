61b036a0fe753c46f99c4339cb934cbc849e4df4
Raghu Betina <raghu@firstdraft.com>
2018-10-20 07:49:07 +0000

    Whitelist columns in Institution for ActiveAdmin

--- app/admin/institutions.rb
+++ app/admin/institutions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Institution do
+
+ permit_params :inst_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
